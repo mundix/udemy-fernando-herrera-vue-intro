0175c88e39c7d446b23601bb306bb738cc162a78
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-08-23 05:41:09 -0400

    Agregando estilo al componente indesicion

--- src/components/Indecision.vue
+++ src/components/Indecision.vue
@@ -1,5 +1,15 @@
 <template>
-  <h1>Indecision</h1>
+  <!-- <h1>Indecision</h1> -->
+  <img src="https://via.placeholder.com/250" alt="">
+  <div class="bg-dark"></div>
+  <div class='indecision-container'>
+      <input type="text" placeholder="Hazme una pregunta">
+      <p>Recuerda termina con un signo de interrogación (?)</p>
+      <div>
+          <h2>Seré millonario</h2>
+          <h1>Si, No, ...</h1>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +18,49 @@ export default {
 }
 </script>
 
-<style>
+<style >
+
+    img, .bg-dark {
+        height: 100vh;
+        left: 0px;
+        max-height: 100%;
+        max-width: 100%;
+        position: fixed;
+        top: 0px;
+        width: 100vw;
+    }
+
+    .bg-dark {
+        background-color: rgba(0, 0, 0, 0.4);
+    }
+
+    .indecision-container {
+        position: relative;
+        z-index: 99;
+    }
+    
+    input {
+        width: 250px;
+        padding: 10px 15px;
+        border-radius: 5px;
+        border: none;
+    }
+    input:focus {
+        outline: none;
+    }
+
+    p {
+        color: white;
+        font-size: 20px;
+        margin-top: 0px;
+    }
+
+    h1, h2 {
+        color: white;
+    }
+    
+    h2 {
+        margin-top: 150px;
+    }
 
 </style>
