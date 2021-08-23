95ba0c7a4626f1c8fcc0b7743be96a40a34a448b
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-08-23 05:32:17 -0400

    Se creao el component Indecision

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- Enviando las props en los componentes el title es el props para el componetn-->
   <!-- Como hacemos par amandar un 15 como numero -->
   <!-- <Counter title='Entraron' start='15'/> -->
   <!-- Si le pongo :start con los 2 puntos -->
   <!-- <Counter title='Entraron' :start='10'/> -->
   <!-- No quiero que los usuarios le pongan valores,  y voy a definir valores por defecto -->
-  <Counter :start='1'/>
+  <!-- <Counter :start='1'/> -->
   <!-- Hay varias forma de manipular estas variables  -->
+  <Indecision/>
 </template>  
 
 <script>
 // el @ hace la referencia a la carpeta src , para no tener que poenr los ../ y asi 
 // import HelloWorld from '@/components/HelloWorld.vue'
 import Counter from './components/Counter.vue'
+import Indecision from './components/Indecision.vue'
 
 export default {
   name: 'App',
   //los compoens que se vayan a usar , en el componetns obj 
   components: {
-    Counter
+    // Counter,
+    Indecision,
   }
 }
 </script>
